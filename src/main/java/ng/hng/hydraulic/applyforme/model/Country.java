22d6a23af396e5fb1a4c0312eeada00dd2e448e7
Ademiju Taiwo <81045677+Ademiju@users.noreply.github.com>
2022-11-13 22:11:29 +0100

    Update Country.java

--- src/main/java/ng/hng/hydraulic/applyforme/model/Country.java
+++ src/main/java/ng/hng/hydraulic/applyforme/model/Country.java
@@ -24,4 +24,14 @@ public class Country {
 
     @Column(name = "abbreviation", nullable = false)
     private String abbreviation;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_on", nullable = false, updatable = false)
+    private Date createdOn;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_on", nullable = false)
+    private Date updatedOn;
 }
